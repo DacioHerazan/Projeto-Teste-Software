1660e5f07f2fd1f02aef95a73408d5ec02d0f49e
Dacio <dacioHerazan2@gmail.com>
2023-05-26 20:44:57 -0300

    tradução

--- src/components/register/register.test.jsx
+++ src/components/register/register.test.jsx
@@ -3,44 +3,33 @@ import Register from "./Register";
 import userEvent from "@testing-library/user-event";
 
 describe("Register component", () => {
+  
   //Teste Unitario
-  it("should render Register component correctly", () => {
+  it("Deve mostrar uma mensagem de erro quando todos os campos não estiverem preenchidos.", async () => {
     render(<Register />);
-    const element = screen.getByRole("heading", { level: 6 });
-    expect(element).toBeInTheDocument();
-  });
-
-  //Teste Unitario
-  it("should show error message when all the fields are not entered", async () => {
-    render(<Register />);
-    const buttonElement = screen.getByRole('button', {  name: /register/i})
+    const buttonElement = screen.getByRole('button', { name: /register/i })
     await userEvent.click(buttonElement); //dispara o botao de submit do form
     const alertElement = screen.getByRole("alert");
     expect(alertElement).toBeInTheDocument();
   });
 
   //Teste Unitario
-  it("should not show any error message when the component is loaded", () => {
+  it("Não deve mostrar nenhuma mensagem de erro quando o componente estiver carregado.", () => {
     render(<Register />);
     const alertElement = screen.queryByRole("alert");
     expect(alertElement).not.toBeInTheDocument();
   });
 
   //Teste Unitario
-  it("should show success message when the registration is successful.", async () => {
+  it("Deve mostrar mensagem quando o cadastro for efetuado com sucesso.", async () => {
     render(<Register />);
-    const buttonElement = screen.getByRole('button', {  name: /register/i})
-    await userEvent.click(buttonElement);
+    const buttonElement = screen.getByRole('button', { name: /register/i })
+    await userEvent.click(buttonElement); //Simula click do usuario
     const alertElement = screen.getByRole("alert");
     expect(alertElement).toBeInTheDocument();
   });
 
-  it("should test for presence of subheading in the component", () => {
-    render(<Register />);
-    const element = screen.getByRole("heading", {
-      name: /please enter your details below to register yourself\./i
-    });
-    expect(element).toBeInTheDocument();
- });
+
+  
 
 });
